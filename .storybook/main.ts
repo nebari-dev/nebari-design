c20166f3842670f737681b78425c5a62da05dd3f
Johnny Bouder <jbouder@openteams.com>
2026-06-17 15:13:33 -0400

    Replace test runner setup with storybook vitest addon.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: ['../stories/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+  ],
   async viteFinal(viteConfig) {
     viteConfig.plugins = [...(viteConfig.plugins ?? []), tailwindcss()];
     viteConfig.resolve = {
